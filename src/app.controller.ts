a31f5e06d02121052ed5ae5bfae5f301b5adfa3c
Carmina Nascimento <cdessana@gmail.com>
2022-02-27 20:41:50 -0400

    chore: add hot reload

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/name')
+  getName(): string{
+    return this.appService.getName();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Mina!';
+    return 'Hello World!';
+  }
+
+  getName(): string{
+    return 'Carmina';
   }
 }
