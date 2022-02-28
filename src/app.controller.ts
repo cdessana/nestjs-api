d5baf880fa1f87f8bdefa0ff761edec6fb13f893
Carmina Nascimento <cdessana@gmail.com>
2022-02-27 20:56:47 -0400

    feat: add endpoint description && status code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 
 @ApiTags('user')
@@ -9,11 +9,15 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get Hello world response' })
+  @ApiResponse({ status: 200, description: 'Hello World Message.' })
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('name')
+  @ApiOperation({ summary: 'Get username response' })
+  @ApiResponse({ status: 200, description: 'carmina.' })
   getName(): string{
     return this.appService.getName();
   }
