a4c74c83980dc0da46e55c793802c0915636ef4c
Carmina Nascimento <cdessana@gmail.com>
2022-03-02 22:16:43 -0400

    feat: add jwt authentication / guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,27 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @ApiTags('user')
 @Controller('user')
 export class AppController {
 
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   @ApiOperation({ summary: 'User login' })
   @ApiResponse({ status: 200, description: 'Login' })
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
